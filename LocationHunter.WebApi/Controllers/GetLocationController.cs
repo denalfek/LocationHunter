6898111cfca943135ed272c60598e8c32b828e7e
denalfek2 <denalfek2@gmail.com>
2019-12-06 16:59:01 +0300

    - added GetLocationController; - changed launchUrl;

---
+++ LocationHunter.WebApi/Controllers/GetLocationController.cs
@@ -0,0 +1,24 @@
+﻿using LocationHunter.WebApi.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationHunter.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetLocationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TryIp()
+        {
+            var ip = HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+
+            if(string.IsNullOrEmpty(ip)) { return Ok("Couldn't parse ip"); }
+
+            return Ok(ip);
+        }
+    }
+}
