2b802d23c82dc8e6ef47dfb8b3226cc4c32b3675
denalfek <denalfek@gmail.com>
2020-07-02 17:42:22 +0300

    - rm MaxMind package

--- LocationHunter.WebApi/Controllers/LocationController.cs
+++ LocationHunter.WebApi/Controllers/LocationController.cs
@@ -1,16 +1,8 @@
-﻿using LocationHunter.Core.Entities;
-using LocationHunter.Dal;
-using LocationHunter.WebApi.Extensions;
+﻿using LocationHunter.WebApi.Extensions;
 using LocationHunter.WebApi.Services.Interfaces;
-using MaxMind.GeoIP2;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace LocationHunter.WebApi.Controllers
